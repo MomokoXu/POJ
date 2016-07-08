f67ce32640a1ffc4087b24777de2601e02e9a976
MomokoXu <momoko0808@hotmail.com>
2016-07-08 16:15:49 -0700

    Update POJ1218

--- POJ1218.java
+++ POJ1218.java
@@ -3,7 +3,28 @@ public class POJ1218 {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 		int numOfLines = input.nextInt();
-
-
+		for (int k = 0; k < numOfLines; k++) {
+			int numOfCells = input.nextInt();
+			boolean[] cellStatus = new boolean[numOfCells + 1];
+			for (int i = 1; i <= numOfCells; i++) {
+				cellStatus[i] = true;
+			}
+			for(int i = 2; i <= numOfCells; i++) {
+				for (int j = i; j <= numOfCells; j+= i) {
+					if (cellStatus[j]) {
+						cellStatus[j] = false;
+					} else {
+						cellStatus[j] = true;
+					}
+				}
+			}
+			int count = 0;
+			for (int i = 1; i <= numOfCells; i++) {
+				if (cellStatus[i]) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
 	}
 }
